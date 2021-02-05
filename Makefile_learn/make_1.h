fb392d57efaae7da53546ac0761012f8dce6bfc1
yizhiqingshu <yizhiqingshufly@gmail.com>
2021-02-05 21:51:56 +0800

    2021.2.5

---
+++ Makefile_learn/make_1.h
@@ -0,0 +1,8 @@
+#ifndef MAKE_1
+#define MAKE_1
+#include <stdio.h>
+#define PRT printf
+#define STR "makefile is ok"
+
+
+#endif
---
+++ Makefile_learn/make_learn_1/make_1.c
@@ -0,0 +1,8 @@
+#include "make_1.h"
+int main()
+{
+    PRT(STR);
+    PRT("Hello world\n");
+    return 0;
+}
+
